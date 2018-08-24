45b8b7ae6f9ea84bb565bef97fb4f0ad0f34a22c
Daedalos97 <sam.j.s.heath@gmail.com>
2018-08-25 00:07:44 +0800

    Set Flight Height

--- frontend/src/Components/AltitudeChart.jsx
+++ frontend/src/Components/AltitudeChart.jsx
@@ -8,7 +8,7 @@ class AltitudeChart extends Component {
             <ResponsiveContainer width='100%' height={400}>
                 <LineChart data={this.props.altitude_data} tool margin={{top: 5, right: 5, bottom: 15, left: 15}} title="Altitude">
                     <Line type="monotone" isAnimationActive={false} dataKey="alti" stroke="black" dot={false}/>
-                    <ReferenceLine y={30} label="Target Height" isFront={true} stroke="red" />
+                    <ReferenceLine y={1300} label="Target Height" isFront={true} stroke="red" />
                     <CartesianGrid strokeDasharray="3 3"/>
                     <XAxis dataKey="time" unit="s" tickCount={2} interval={'preserveEnd'} allowDecimals={false}><Label value="Time Since Launch Sequence Began (s)" position="bottom" offset={0}/></XAxis>
                     <YAxis label={{value: "Height", angle: -90, position: 'left', offset: 0}} unit="m"/>
